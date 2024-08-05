3440a551959b5197c5092ef2bb908984ac6f1b6d
Pawanpathariya <pawanpathariys@gmail.com>
2024-08-05 07:23:01 +0530

    add

---
+++ equal_or_not.cpp
@@ -0,0 +1,10 @@
+//The number are equal or not
+#include<iostream>
+using namespace std;
+int main(){
+    int num1,num2;
+    cout<<"Enter Two number : ";
+    cin>>num1>>num2;
+
+    num1==num2 ? cout<<"Numbers are equal" : cout<<"Numbers are not equal";
+}
