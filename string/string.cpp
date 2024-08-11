75a32c2ab13dc2e3a651d878d7a44a716e65f414
Pawanpathariya <pawanpathariys@gmail.com>
2024-08-11 08:47:14 +0530

    adding question for string basic

---
+++ string/string.cpp
@@ -0,0 +1,22 @@
+
+#include<iostream>
+using namespace std;
+ int main(){ 
+//     char name[6]={'a','r','j','u','n'}; // default take null character string termination
+//     cout<<name<<endl;
+//     //size = length +1;
+//     char name1[10]={'a','r','j','u','n','\0'};
+//     cout<<name1<<endl;
+
+//     char name2[6]="arjun";
+//     cout<<name2<<endl;
+    
+//     string name3="arjun";
+//     cout<<name3<<endl;
+ string name;
+ cout<<"Enter your name ";
+ getline(cin,name);
+ //getline(input_object,buffer)
+ cout<<name;
+
+}
