32cc8439041f777e16701b9cce32bf0e381ffb35
Pawanpathariya <pawanpathariys@gmail.com>
2024-08-15 09:38:24 +0530

    Adding question for sum of two number using pointer

---
+++ pointer/add2num.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+int n1,n2;
+cout<<"Enter two number ";
+cin>>n1>>n2;
+int *p1=&n1;
+int *p2=&n2;
+
+cout<<"The sum of both number is :"<<*p1+*p2;
+}
