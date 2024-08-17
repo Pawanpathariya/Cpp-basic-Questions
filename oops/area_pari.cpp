d77e599504246a34a6022842fe5ea2a83057ee92
Pawanpathariya <pawanpathariys@gmail.com>
2024-08-17 08:11:36 +0530

    adding constructor basic question

---
+++ oops/area_pari.cpp
@@ -0,0 +1,21 @@
+//Write a c++ class named Circle constructed by a radius and two methods which will compute the area and the perimeter of a circle
+#include<iostream>
+using namespace std;
+class Circle{
+public:
+int radius;
+
+void insert (){
+    cout<<"enter a radius of circle :";
+    cin>>radius;
+}
+void area(){
+    cout<<"Area of cicle is "<<3.14*radius*radius<<endl;
+}
+
+void parameter(){
+    cout<<"Parameter of circle is "<<2*3.14*radius<<endl;
+}
+
+}obj1;
+
