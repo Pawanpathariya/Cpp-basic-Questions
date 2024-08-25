99b24a4862b02b2934ee4100b9b493bc4936640d
Pawanpathariya <pawanpathariys@gmail.com>
2024-08-25 10:58:06 +0530

    inheritance basic

---
+++ oops/inheritance.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+class Parent{
+private:
+void f1(){
+    cout<<"This is s function f1 of class Parent\n";
+}
+protected:
+void f2(){
+    cout<<"This is s function f2 of class Parent \n";
+}
+public:
+void f3(){
+    cout<<"This is s function f3 of class Parent \n";
+}
+};
+
