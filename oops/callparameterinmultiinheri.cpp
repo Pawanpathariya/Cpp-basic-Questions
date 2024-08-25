861b7fa4e597d72ac4ea485afe9d505272355c1e
Pawanpathariya <pawanpathariys@gmail.com>
2024-08-25 10:59:39 +0530

    inheritance basic

--- oops/callparameterinmultiinheri.cpp
+++ oops/callparameterinmultiinheri.cpp
@@ -1,3 +1,16 @@
 // calling base class constructor in derived class
 #include<iostream>
 using namespace std;
+
+	class University{
+					protected:
+						string university_name;
+						int number_of_college_affilated;
+					public:
+						University(string un, int numC){
+							university_name=un;
+							number_of_college_affilated=numC;
+						}
+						
+	};
+		
