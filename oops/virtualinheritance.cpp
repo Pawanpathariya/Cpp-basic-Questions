21a39752f0750a78f7a883806201342b6fd6ab83
Pawanpathariya <pawanpathariys@gmail.com>
2024-08-25 11:01:43 +0530

    inheritance basic

--- oops/virtualinheritance.cpp
+++ oops/virtualinheritance.cpp
@@ -11,3 +11,17 @@ class Parent1: virtual public GrandParent{
 class Parent2:virtual public GrandParent{
 
 };
+class Child : public Parent1, public Parent2{
+
+};
+int main(){
+cout<<sizeof(GrandParent)<<endl;
+cout<<sizeof(Parent1)<<endl;
+cout<<sizeof(Parent2)<<endl;
+cout<<sizeof(Child)<<endl;
+Child obj;
+obj.number=90;
+obj.Parent1::number=44;
+obj.Parent2::number=84;
+cout<<obj.number<<endl;
+}
